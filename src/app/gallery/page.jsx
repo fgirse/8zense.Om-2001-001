090f2a7ddfb158c858a6fe34833c69e5bdb1701e
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-01-10 10:14:23 +0100

    4er commit

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import MuxVideo from "@/components/"
 const page = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-center text-amber-500 text-3xl md:text-7xl lg:text-5x">
+      <h1 className="text-center text-blue-500 text-3xl md:text-7xl lg:text-5x">
         Galleria
       </h1>
     </div>
